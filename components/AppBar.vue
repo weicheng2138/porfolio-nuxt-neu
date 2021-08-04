1dcbf3f1103ace36b44456683e085eab174ab38d
weicheng <weicheng2138@gmail.com>
2021-08-04 17:26:19 +0800

    readme update

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -141,7 +141,7 @@ export default {
 
 <style lang="scss" scoped>
 nav {
-    @apply fixed flex justify-between items-center w-full font-mono h-bcNavHeight bg-bcColorTitle opacity-90
+    @apply fixed flex justify-between items-center w-full font-mono h-bcNavHeight bg-bcColorTitle opacity-80
     px-6 md:px-10 lg:px-12 backdrop-filter backdrop-blur-sm shadow-appbar transition-all;
 
     .logo {
